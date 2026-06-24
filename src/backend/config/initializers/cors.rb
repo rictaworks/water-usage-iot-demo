471b3e078208285d65277fad4c7156e63951a068
Ricta Works <takizawa@rictaworks.jp>
2026-06-25 06:00:02 +0900

    Fix CORS: add water-usage-iot-demo.rictaworks.jp to allowed origins (#3)

--- src/backend/config/initializers/cors.rb
+++ src/backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://*.vercel.app'
+    origins 'http://localhost:3000', 'https://*.vercel.app', 'https://water-usage-iot-demo.rictaworks.jp'
     resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
   end
 end
